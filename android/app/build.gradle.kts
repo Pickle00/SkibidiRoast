1725520f17c9b01ff2dffb6090f32ba4291e47fc
Pickle00 <rumba803@gmail.com>
2025-11-30 16:05:57 +0545

    feat: native splash screen setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.skibidi_roast"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
